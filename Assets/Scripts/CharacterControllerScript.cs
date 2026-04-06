dd9466c4334c517242fd6ea10a3936c1233c2de2
nominal251 <jackhardy524@gmail.com>
2026-04-06 17:48:26 -0400

    Start gameplay prototype

---
+++ Assets/Scripts/CharacterControllerScript.cs
@@ -0,0 +1,140 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+
+public class CharacterControllerScript : MonoBehaviour
+{
+    private CharacterController controller;
+
+    // Input Actions
+    public InputAction moveAction;
+    //public InputAction lookAction;
+    public InputAction sprintAction;
+
+    //private Vector2 lookInput;
+    private Vector2 moveInput;
+
+    //public float mouseSens = 1;
+    public float speed = 4.0f;
+    public float sprintSpeed = 7.0f;
+    public float gravity = -9.8f;
+    Vector3 velocity;
+
+    public Transform mainCamera;
+
+    //private float xRotation;
+
+    public float maxSprint = 8f;
+    public float drainRate = 1f;
+    public float rechargeRate = 0.5f;
+    public float sprintDelay = 2f;
+
+    private float sprint;
+    private float counter;
+
+    public GameObject sprintBar;
+    private Image sprintBarImage;
+
+    void OnEnable()
+    {
+        moveAction.Enable();
+        //lookAction.Enable();
+        sprintAction.Enable();
+    }
+
+    void OnDisable()
+    {
+        moveAction.Disable();
+        //lookAction.Disable();
+        sprintAction.Disable();
+    }
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+
+        sprintBarImage = sprintBar.GetComponent<Image>();
+
+        sprint = maxSprint;
+    }
+
+    void Update()
+    {
+        Movement();
+        //Look();
+        Gravity();
+
+        Recharge();
+
+        sprintBarImage.fillAmount = sprint / maxSprint;
+
+        if (Keyboard.current.escapeKey.wasPressedThisFrame)
+            Application.Quit();
+    }
+
+    void Movement()
+    {
+        moveInput = moveAction.ReadValue<Vector2>();
+
+        float forwardInput = moveInput.y;
+        float sideInput = moveInput.x;
+
+        Vector3 move = (transform.right * sideInput + transform.forward * forwardInput).normalized;
+
+        if (sprintAction.IsPressed() && forwardInput > 0 && sprint > 0)
+        {
+            controller.Move(move * sprintSpeed * Time.deltaTime);
+            sprint -= drainRate * Time.deltaTime;
+            counter = 0;
+        }
+        else
+        {
+            controller.Move(move * speed * Time.deltaTime);
+        }
+    }
+
+    /*void Look()
+    {
+        lookInput = lookAction.ReadValue<Vector2>();
+
+        float lookX = lookInput.x * mouseSens;
+        float lookY = lookInput.y * mouseSens;
+
+        transform.Rotate(Vector3.up * lookX);
+
+        xRotation -= lookY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        mainCamera.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+    }*/
+
+    void Gravity()
+    {
+        if (controller.isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+    }
+
+    void Recharge()
+    {
+        if (sprint >= maxSprint)
+        {
+            sprint = maxSprint;
+            return;
+        }
+
+        counter += Time.deltaTime;
+
+        if (counter >= sprintDelay)
+        {
+            sprint += rechargeRate * Time.deltaTime;
+        }
+    }
+}

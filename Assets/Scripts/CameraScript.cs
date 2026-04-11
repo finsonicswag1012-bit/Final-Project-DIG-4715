d28cc53af34034f46ccb65e2eea364c085e4cf7c
nominal251 <jackhardy524@gmail.com>
2026-04-11 04:23:33 -0400

    Camera adjustments

--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -4,16 +4,19 @@ public class CameraScript : MonoBehaviour
 {
     public Transform target;
     public float ySmoothTime = 0.2f;
+    public float horizontalSmoothTime = 0.05f;
 
     float yVelocity;
+    float xVelocity;
+    float zVelocity;
 
     void LateUpdate()
     {
         Vector3 pos = transform.position;
 
         pos.y = Mathf.SmoothDamp(pos.y, target.position.y, ref yVelocity, ySmoothTime);
-        pos.x = target.position.x;
-        pos.z = target.position.z;
+        pos.x = Mathf.SmoothDamp(pos.x, target.position.x, ref xVelocity, horizontalSmoothTime);
+        pos.z = Mathf.SmoothDamp(pos.z, target.position.z, ref zVelocity, horizontalSmoothTime);
 
         transform.position = pos;
     }
